8724fcea17e49b4bcb4934f653e0ec7fdb215aa3
patricia <pvalenci22@jaumebalmes.net>
2023-01-23 20:29:19 +0100

    part A amb comentaris

--- exercici2_A.py
+++ exercici2_A.py
@@ -1,5 +1,10 @@
+"""diccionari de les altures de diferents alumnes"""
 altura={'Patri':170, 'Alex':189,'Har':158,'David':180,'Luis':160,'Itamar':173}
+""" longitud del diccionari"""
 print (len(altura))
+"""mostra nomes els valors"""
 print (altura.values())
+"""aqui mostrarà només el valor que s'elimina"""
 print(altura.popitem())
+"""i aqui el diccionari sense el valor últim que s'ha eliminat"""
 print(altura)
